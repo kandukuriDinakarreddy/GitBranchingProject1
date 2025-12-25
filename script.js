10f3cd131af5497bb579af1ed7217c8884fa8c62
Dinakar Reddy <dinakar@example.com>
2025-12-25 23:08:38 +0530

    Added image and video preview feature

--- script.js
+++ script.js
@@ -6,9 +6,24 @@ function checkFile() {
         result.innerText = "Please upload a file first.";
         return;
     }
+// Show preview
+    if (file.type.includes("image")) {
+        previewImg.src = URL.createObjectURL(file);
+        previewImg.style.display = "block";
+        previewVid.style.display = "none";
+    } 
+    else if (file.type.includes("video")) {
+        previewVid.src = URL.createObjectURL(file);
+        previewVid.style.display = "block";
+        previewImg.style.display = "none";
+    } 
+    else {
+        result.innerText = "Unsupported file type.";
+        return;
+    }
 
     const fileSizeMB = file.size / (1024 * 1024);
-    const fileType = file.type;
+    
 
     if (fileType.includes("image") || fileType.includes("video")) {
 
